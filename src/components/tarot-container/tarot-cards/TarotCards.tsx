49f3adbd2200081cef708164c4283b5d02c0281c
v1tal4ik <vetalgta@gmail.com>
2024-06-24 22:09:51 +0300

    add responsive

--- src/components/tarot-container/tarot-cards/TarotCards.tsx
+++ src/components/tarot-container/tarot-cards/TarotCards.tsx
@@ -2,7 +2,7 @@ import TarotCardsPath from 'src/assets/images/tarot-cards.svg';
 
 const TarotCards = () => {
 	return (
-		<div className='w-full flex flex-col justify-center items-center gap-y-10'>
+		<div className='w-full flex flex-col justify-center items-center gap-y-4'>
 			<h3 className='w-[75%] text-xl text-center font-semibold max-sm:text-base max-sm:w-full'>
 				Let’s check what awaits you in career and finances in the near future
 			</h3>
